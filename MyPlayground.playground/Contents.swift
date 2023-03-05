3d3f6f7f6780b49cb81c4f3d3d7c7e10889990c6
Vượng <nhanvuongdesigner@gmail.com>
2023-03-05 12:56:32 +0700

    Initial Commit

---
+++ MyPlayground.playground/Contents.swift
@@ -0,0 +1,56 @@
+import UIKit
+
+//Day 1
+
+var first = "Hello XCode" // var create variable, in variable has value "Hello XCode"
+first = "Hello Apple" // change value: "Hello Xcode" to "Hello Apple"
+var second = "It's me"
+second = "the second changed"
+var third = "Third arrive"
+third = "Third dissapear"
+var my_age = 20 // number type Int (Integer)
+var future_age = 100_000_000 // :)))))
+
+//Multiple-strings
+var triple_text = """
+First
+Second
+Third
+""" // Shows single or multiple string or int line
+var lmao = """
+Hello \
+Hallo \
+Hulle
+""" // use \ to include string or int in single line
+
+var number = """
+4 \
+8 \
+16
+""" //Noice, let's continue
+
+//Double
+var pi = 3.14159
+
+//String interpolation
+var temparture_today = "38*C"
+var Result = "The tempature today is: \(temparture_today)"
+var Display = "Our news today: \(Result)"
+
+//Constants
+let me = "forever 20"
+
+//Type Anotation
+var name: String
+var age: Int
+var male: Bool
+var height: Int
+
+name = "Nhan Vuong"
+age = 20
+male = true
+height = 170
+
+//Day 2
+
+
