63713d9bfbfe0dbdc5425c9d13f52839e3fbd918
Vượng <nhanvuongdesigner@gmail.com>
2023-03-06 22:11:07 +0700

    Day 3: hell of a ride

--- MyPlayground.playground/Pages/Untitled Page.xcplaygroundpage/Contents.swift
+++ MyPlayground.playground/Pages/Untitled Page.xcplaygroundpage/Contents.swift
@@ -162,3 +162,118 @@ enum Calendar: Int {
 let date = Calendar(rawValue: 4)
 
 //Day 2 end, phewww
+
+//Day 3 begin with operator & conditions
+
+//Arithmetic operator - 4 phep tinh + - * /
+let numberone = 16
+let numbertwo = 8
+
+let total = numberone + numbertwo
+let minus_result = numberone - numbertwo
+
+let phepnhan = numberone * numbertwo
+let phepchia = numberone / numbertwo
+
+let remainder = 10 % numbertwo
+
+
+//Operator overload
+
+let consomot = 1
+let consohai = 1 + 2 //dung Int value
+
+let chucaidau = "mot"
+let chucaihai = chucaidau + "hai" //dung string value
+
+let array1 = ["ba", "bon"]
+let array2 = ["nam", "sau"]
+let tong_array = array1 + array2 //dung array
+
+//Compount assignment operator
+
+var diembandau = 95
+diembandau -= 5
+diembandau += 15
+diembandau *= 10
+diembandau /= 5 //ghan tac vu vao, nhu phep tinh + - * /
+
+var caunoi = "Cau lam sao the"
+caunoi += ",cau co sao khong" // them strings vao value
+
+//Comparation operator - phep so sanh
+
+let somot = 2
+let sohai = 3
+
+somot < sohai
+sohai < somot
+somot == sohai
+somot != sohai
+
+let uid = "so mot"
+let uxd = "so so much"
+
+uid > uxd //so sanh 2 strings
+
+//Condition -  dung if va else
+
+let ly = 123
+let duc = 100
+if ly + duc == 324 {print("ducvoily")} else if duc > ly { print("duc lon hon ly")} else {("duc thap hon ly")}
+
+//Combine conditions
+
+if ly == 123 && duc == 100 { print("duc_va_ly")}
+if ly > 123 || duc == 100 {print("duc khac ly")}
+
+//The ternary operator
+
+let firstCard = 11
+let secondCard = 10
+print(firstCard == secondCard ? "Cards are the same" : "Cards are different")
+
+//Co the dung if else nhu nay
+if firstCard == secondCard {
+    print("Cards are the same")
+} else {
+    print("Cards are different")
+}
+
+//Switch statement - dung khi co nhieu menh de if else
+
+let weather = "sunny"
+
+switch weather {
+case "rain":
+    print("Bring an umbrella")
+case "snow":
+    print("Wrap up warm")
+case "sunny":
+    print("Wear sunscreen")
+    fallthrough
+default:
+    print("Enjoy your day!")
+}
+
+//Range operator - 2 khoang cach
+
+let score = 49
+
+switch score {
+case 0..<50:
+    print("You failed badly.")
+case 50..<85:
+    print("You did OK.")
+default:
+    print("You did great!")
+}
+
+// You’ve made it to the end of the third part of this series, so let’s summarize:
+
+//Swift has operators for doing arithmetic and for comparison; they mostly work like you already know.
+//There are compound variants of arithmetic operators that modify their variables in place: +=, -=, and so on.
+//You can use if, else, and else if to run code based on the result of a condition.
+//Swift has a ternary operator that combines a check with true and false code blocks. Although you might see it in other code, I wouldn’t recommend using it yourself.
+//If you have multiple conditions using the same value, it’s often clearer to use switch instead.
+//You can make ranges using ..< and ... depending on whether the last number should be excluded or included.
